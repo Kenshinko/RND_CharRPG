670e46cf005c8e1d52949fb96ba5759bed7678ff
Курилов Евгений <evgeniy.kurilov@gazprombank.ru>
2026-01-20 18:18:21 +0300

    first release

---
+++ src/index.js
@@ -0,0 +1,109 @@
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	Client,
+	EmbedBuilder,
+	Events,
+	GatewayIntentBits,
+	StringSelectMenuBuilder,
+} from 'discord.js';
+import 'dotenv/config';
+import { GoogleSheetsService } from './googleSheets.js';
+
+const TOKEN = process.env.DISCORD_TOKEN;
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent, // Для чтения содержимого сообщений
+	],
+});
+
+let LISTS = [];
+// ==================================================================================== //
+client.once(Events.ClientReady, async (readyClient) => {
+	console.log('Привет');
+	try {
+		const data = await GoogleSheetsService.getLists();
+		const result = data.map(({ properties }) => {
+			return { id: properties.sheetId, name: properties.title };
+		});
+
+		LISTS = [...result];
+		console.log(LISTS);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+client.on(Events.MessageCreate, async (message) => {
+	if (message.author.bot) return;
+
+	if (message.content === '/rnd') {
+		try {
+			// Текстовое окно
+			const rndEmbed = new EmbedBuilder()
+				.setColor(0x0099ff)
+				.setTitle('🌟 Выбор списка')
+				.setDescription('Выберите список, из которого случайно будет выбран персонаж.');
+
+			// Выпадающий список элеметов
+			const selectMenu = new StringSelectMenuBuilder()
+				.setCustomId('selectLists')
+				.setPlaceholder('Выберите список...')
+				.addOptions(LISTS);
+
+			// Кнопка подтверждения
+			const button = new ButtonBuilder()
+				.setCustomId('selectConfirm')
+				.setLabel('ОК')
+				.setStyle(ButtonStyle.Primary)
+				.setEmoji('🎲');
+
+			// Рендер компонентов
+			const actionRow1 = new ActionRowBuilder().addComponents(selectMenu);
+			const actionRow2 = new ActionRowBuilder().addComponents(button);
+
+			// Слушатель события
+			await message.channel.send({
+				embeds: [rndEmbed],
+				components: [actionRow1, actionRow2],
+			});
+		} catch (error) {
+			console.error('Ошибка отправки сообщения: ', error);
+			await message.reply('❌ Произошла ошибка при создании меню выбора');
+		}
+	}
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (interaction.isStringSelectMenu() && interaction.customId === 'selectLists') {
+		const selectedList = interaction.values[0];
+		client.userSelections = selectedList;
+	}
+
+	if (interaction.isButton() && interaction.customId === 'confirmSelection') {
+		const selectedList = client.userSelections?.get(interaction.user.id);
+
+		if (!selectedList) {
+			await interaction.reply({
+				content: '❌ Сначала выберите список из меню!',
+				flags: MessageFlags.Ephemeral, // Только для отправителя
+			});
+
+			return;
+		}
+
+		// Здесь добавить логику выбора случайного персонажа
+
+		await interaction.reply({
+			content: `✅ Вы выбрали список: **${selectedList}**`,
+			flags: MessageFlags.Ephemeral, // Только для отправителя
+		});
+
+		client.userSelections?.delete(interaction.user.id);
+	}
+});
+// ==================================================================================== //
+client.login(TOKEN);
